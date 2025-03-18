cbf0351ead39701a1dcdef345964ef6cfe1e4356
leticiamarts <leticiamarts99@gmail.com>
2025-03-18 12:32:25 -0300

    chore: atualiza docstring da função validar_cpf

--- modules/cpf.py
+++ modules/cpf.py
@@ -1,5 +1,16 @@
 def validar_cpf(cpf: str) -> bool:
-    """Verifica se um CPF contém exatamente 11 dígitos numéricos."""
+    """Verifica se um CPF contém exatamente 11 dígitos numéricos.
+    
+    Args:
+        cpf (str): O CPF a ser validado.
+
+    Returns:
+        bool: True se o CPF for válido, caso contrário, levanta uma exceção.
+
+    Raises:
+        TypeError: Se o CPF não for uma string.
+        ValueError: Se o CPF não contiver apenas números ou não tiver exatamente 11 dígitos.
+    """
     if not isinstance(cpf, str):
         raise TypeError("O CPF deve ser uma string.")
     
